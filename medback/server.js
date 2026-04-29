37aa5fd9364f3fb1f0748119670c34f8d939b578
Saheb <sahebpan00@gmail.com>
2026-04-29 19:54:21 +0530

    Updated API URL to deployed backend

--- medback/server.js
+++ medback/server.js
@@ -1,23 +1,27 @@
 require("dotenv").config();
-const express=require('express');
-const dotenv=require('dotenv')
-const chatRoute=require('./routes/chat')
+const express = require('express');
+const dotenv = require('dotenv')
+const chatRoute = require('./routes/chat')
 
 
 dotenv.config(); // to generate the process.env object notation 
-const cors=require('cors'); 
+const cors = require("cors");
 
-const userRoute=require('./routes/userRoute');
-const doctorRoute=require('./routes/doctorRoute');
+app.use(cors({
+    origin: "https://medical-app-five-chi.vercel.app"
+}));
+
+const userRoute = require('./routes/userRoute');
+const doctorRoute = require('./routes/doctorRoute');
 const appointmentRouter = require("./routes/appointmentRouter");
 
 
-const app=express(); 
+const app = express();
 
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false })); 
+app.use(express.urlencoded({ extended: false }));
 console.log("userRoute:", typeof userRoute);
 console.log("doctorRoute:", typeof doctorRoute);
 console.log("appointmentRouter:", typeof appointmentRouter);
@@ -26,18 +30,18 @@ console.log("GROQ KEY:", process.env.GROQ_API_KEY);
 
 //register route 
 
-app.use('/med',userRoute);
-app.use('/doctor',doctorRoute);
+app.use('/med', userRoute);
+app.use('/doctor', doctorRoute);
 app.use('/appointments', appointmentRouter);
 app.use('/api/chat', chatRoute);
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Hello from JWT3 App Server');
 });
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`server is running in the port ${PORT}`);
 });
